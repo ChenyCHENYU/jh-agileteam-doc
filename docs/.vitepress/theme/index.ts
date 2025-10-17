13328ce1cd9a9581436dab6ae517782f7b356ea8
ChenYu <ycyplus@gmail.com>
2025-10-17 09:20:54 +0800

    perf: 评论插件配置部分优化

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,9 +2,9 @@
  * @Author: ChenYu ycyplus@gmail.com
  * @Date: 2025-10-15 08:46:20
  * @LastEditors: ChenYu ycyplus@gmail.com
- * @LastEditTime: 2025-10-17 08:58:16
+ * @LastEditTime: 2025-10-17 09:00:18
  * @FilePath: \jh-agileteam-doc\docs\.vitepress\theme\index.ts
- * @Description:
+ * @Description: VitePress 主题入口文件
  * Copyright (c) 2025 by CHENY, All Rights Reserved 😎.
  */
 import DefaultTheme from "vitepress/theme";
@@ -38,17 +38,17 @@ export default {
     // Waline 评论系统配置
     const walinePlugin = useWalineComments({
       serverURL: "https://waline-comment-lilac.vercel.app",
-      meta: ["nick", "mail"], // 只需要姓名和邮箱
-      requiredMeta: ["nick", "mail"], // 姓名和邮箱都必填
+      meta: ["nick", "mail"],
+      requiredMeta: ["nick", "mail"],
       login: "enable",
       wordLimit: [0, 500],
       pageSize: 10,
       search: false,
-      imageUploader: false, // 禁用图片上传
+      noCopyright: true, // 隐藏底部版权信息 "Powered by Waline"
       mountDelay: 800,
 
       locale: {
-        placeholder: "💬 欢迎评论（支持 Markdown 语法）",
+        placeholder: "💬 欢迎评论（支持 Markdown 语法，提交后正确渲染）",
         sofa: "来发表第一条评论吧~",
         nick: "姓名或工号",
         nickError: "请填写姓名或工号",
